280ec5cdd63d6e4217e685c98c58ee90dc4c3845
ViolaDeRuvo <viola.deruvo@studbocconi.it>
2025-01-14 19:05:16 +0100

    Add files via upload

---
+++ plot_all.py
@@ -0,0 +1,178 @@
+import sys
+sys.path.append('../')
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+from utils_plotting import set_axis_color
+from utils_plotting import custom_colors
+from utils_plotting import remove_xticklabel
+plt.style.use('../thesis_mplrc.dms')
+
+# Parse arguments from the command line
+parser = argparse.ArgumentParser()
+parser.add_argument("-datadir", type=str, help="data directory", default=".")
+parser.add_argument("-resultdir", type=str, help="result directory", default=".")
+parser.add_argument("-connect_type", type=str, help="connection type", default=".")
+parser.add_argument("-outfile_name", type=str, help="name of output file", default=".")
+parser.add_argument("-durex", type=float, help="duration of example")
+parser.add_argument("-alpha", type=float, help="moving average time scale")
+parser.add_argument("-numberofrealiz", type=int, help="number of realization of the noise", default=1)
+
+args = parser.parse_args()
+number_of_realizations = args.numberofrealiz
+
+# ~~~~~~~~~~~
+# Import data
+# ~~~~~~~~~~~
+# Wsum (average of nonzero synaptic weights
+wsum = np.loadtxt('{}/plasticity_rule.0.wsum_seed{}'.format(args.datadir, 1))
+for i in range(1, number_of_realizations):
+    filename = '{}/plasticity_rule.0.wsum_seed{}'.format(args.datadir, i+1)
+    wsum = np.hstack([wsum, np.reshape(np.loadtxt(filename)[:, 1], (-1,1))])  # average of nonzero synaptic weights
+wsum_mean = np.mean(wsum[:, 1:], axis=1)
+wsum_std = np.std(wsum[:, 1:], axis=1)
+
+# Event and burst rate of the pre and postsynaptic neurons
+brate_post = np.loadtxt('{}/plasticity_rule.0.brate_seed{}'.format(args.datadir, 1))
+for i in range(1, number_of_realizations):
+    filename = '{}/plasticity_rule.0.brate_seed{}'.format(args.datadir, i+1)
+    brate_post = np.hstack([brate_post, np.loadtxt(filename)[:, 1:]])  # BR and ER of postsynaptic population
+brate_post_meanBR, brate_post_meanER = np.mean(brate_post[:, 1::2], axis=1), np.mean(brate_post[:, 2::2], axis=1)
+brate_post_stdBR, brate_post_stdER = np.std(brate_post[:, 1::2], axis=1), np.std(brate_post[:, 2::2], axis=1)
+brate_post_meanBP = np.mean(brate_post[:, 1::2]/brate_post[:, 2::2], axis=1)
+brate_post_stdBP = np.std(brate_post[:, 1::2]/brate_post[:, 2::2], axis=1)
+
+brate_pre = np.loadtxt('{}/plasticity_rule.0.brate_input_seed{}'.format(args.datadir, 1))
+for i in range(1, number_of_realizations):
+    filename = '{}/plasticity_rule.0.brate_input_seed{}'.format(args.datadir, i+1)
+    brate_pre = np.hstack([brate_pre, np.loadtxt(filename)[:, 1:]])  # BR and ER of presynaptic population
+brate_pre_meanBR = np.mean(brate_pre[:, 1::2], axis=1)
+brate_pre_meanER = np.mean(brate_pre[:, 2::2], axis=1)
+brate_pre_meanBP = np.mean(brate_pre[:, 1::2]/brate_pre[:, 2::2], axis=1)
+brate_pre_stdBR = np.std(brate_pre[:, 1::2], axis=1)
+brate_pre_stdER = np.std(brate_pre[:, 2::2], axis=1)
+brate_pre_stdBP = np.std(brate_pre[:, 1::2]/brate_pre[:, 2::2], axis=1)
+
+# construct average of moving average BP
+all_er_traces = []
+all_br_traces = []
+for r in range(1, number_of_realizations+1):
+    er_trace = np.loadtxt(args.datadir + '/plasticity_rule0.0.trevent_seed' + str(r))
+    br_trace = np.loadtxt(args.datadir + '/plasticity_rule0.0.trburst_seed' + str(r))
+    for i in range(1, 50):
+        er_tmp = np.loadtxt(args.datadir + '/plasticity_rule'+str(i)+'.0.trevent_seed'+str(r))
+        br_tmp = np.loadtxt(args.datadir + '/plasticity_rule'+str(i)+'.0.trburst_seed'+str(r))
+        er_trace[:, 1] += er_tmp[:, 1]
+        br_trace[:, 1] += br_tmp[:, 1]
+    all_er_traces.append(er_trace[:, 1] / 50.)
+    all_br_traces.append(br_trace[:, 1] / 50.)
+all_er_traces = np.array(all_er_traces)
+all_br_traces = np.array(all_br_traces)
+mean_ER_trace = np.mean(all_er_traces, axis=0)
+mean_BR_trace = np.mean(all_br_traces, axis=0)
+mean_BP_trace = np.mean(all_br_traces/all_er_traces, axis=0)
+std_BP_trace = np.std(all_br_traces/all_er_traces, axis=0)
+
+# ~~~~~~~~
+# Plotting
+# ~~~~~~~~
+n_SD = 1
+fig = plt.figure(figsize=(70./25.4, 88./25.4))
+tics = list(4*args.alpha + args.durex*np.arange(0, 7))
+binSize = brate_post[1, 0] - brate_post[0, 0]
+
+ax1 = fig.add_subplot(311)
+ax1.plot(brate_post[:, 0], brate_post_meanER, color=custom_colors['blue'], lw=1, label='ER')
+
+print("Max Value of ER_post")
+index_titti = np.argmin(np.abs(brate_post[:, 0] - 20))
+value_titti = brate_post_meanER[index_titti]
+print(value_titti)
+
+
+ax1.fill_between(brate_post[:, 0],
+                 brate_post_meanER - n_SD*brate_post_stdER,
+                 brate_post_meanER + n_SD*brate_post_stdER,
+                 color=custom_colors['blue'], alpha=0.5, lw=0)
+ax1.plot(brate_post[:, 0], np.ones_like(brate_post_meanER)*np.mean(brate_post_meanER[int(args.alpha/binSize):int((3*args.alpha + args.durex)/binSize)]), ':', color="black", lw=0.5)
+ax1.set_ylabel('Postsyn. ER [Hz]', color=custom_colors['blue'])
+ax1.set_ylim([0, 15])
+ax1.set_yticks([0,5,10,15])
+ax1.spines['top'].set_visible(False)
+ax1.set_xticks([0]+tics)
+remove_xticklabel(ax1)
+
+ax1_tw = ax1.twinx()
+ax1_tw.plot(brate_post[:, 0], 100*brate_post_meanBP, color=custom_colors['red'], lw=1)
+
+print("Max Vlue of BP")
+print(np.nanmax(100*brate_post_meanBP))
+
+ax1_tw.fill_between(brate_post[:, 0],
+                    100*brate_post_meanBP - n_SD*100*brate_post_stdBP,
+                    100*brate_post_meanBP + n_SD*100*brate_post_stdBP,
+                    color=custom_colors['red'], alpha=0.5, lw=0)
+ax1_tw.plot(er_trace[:, 0], 100*mean_BR_trace/mean_ER_trace, '--', color=custom_colors['red'], lw=1, label=r'$\overline{P}$')
+ax1_tw.fill_between(er_trace[:, 0],
+                    100*mean_BP_trace - n_SD*100*std_BP_trace,
+                    100*mean_BP_trace + n_SD*100*std_BP_trace,
+                    color=custom_colors['red'], alpha=0.5, lw=0)
+ax1_tw.set_yticks([0, 50])
+ax1_tw.set_ylim([0, 60])
+ax1_tw.legend(loc='upper right', bbox_to_anchor=(1, 1.1))
+ax1_tw.set_ylabel('Postsyn. BP [%]', color=custom_colors['red'])
+ax1_tw.spines['top'].set_visible(False)
+
+ax2 = fig.add_subplot(312, sharex=ax1)
+ax2.plot(wsum[:, 0], 100*(wsum_mean - wsum_mean[0])/wsum[0, 1], color='black', lw=1.5, label='Weight change')
+ax2.fill_between(wsum[:, 0], 100*(wsum_mean - wsum_mean[0])/wsum[0, 1] - 100*n_SD*wsum_std/wsum[0, 1],
+                 100*(wsum_mean - wsum_mean[0])/wsum[0, 1] + 100*n_SD*wsum_std/wsum[0, 1],
+                 color='black', alpha=0.5, lw=0)
+
+print("Max value of DW")
+print(np.nanmax(100*(wsum_mean - wsum_mean[0])/wsum[0, 1]))
+
+ax2.plot(wsum[:, 0], 100*(brate_post_meanBP - mean_BP_trace), '-.', lw=1,
+         color=custom_colors['red'], label=r'Postsyn. BP$ - \overline{P}$')
+ax2.fill_between(wsum[:, 0],
+                 100*(brate_post_meanBP - mean_BP_trace) - n_SD*100*np.sqrt(brate_post_stdBP**2 + std_BP_trace**2),
+                 100*(brate_post_meanBP - mean_BP_trace) + n_SD*100*np.sqrt(brate_post_stdBP**2 + std_BP_trace**2),
+                 color=custom_colors['red'], alpha=0.5, lw=0)
+ax2.plot(wsum[:, 0], np.zeros_like(wsum[:, 0]), ':', color="black", lw=0.5)
+ax2.set_ylabel('[%]')
+ax2.legend(loc='lower left', bbox_to_anchor=(1, 1.05))
+ax2.spines['top'].set_visible(False)
+ax2.spines['right'].set_visible(False)
+remove_xticklabel(ax2)
+
+ax3 = fig.add_subplot(313)
+ax3.plot(brate_pre[:, 0], brate_pre_meanER, color=custom_colors['blue'], lw=1, label='ER')
+ax3.fill_between(brate_pre[:, 0],
+                 brate_pre_meanER - n_SD*brate_pre_stdER,
+                 brate_pre_meanER + n_SD*brate_pre_stdER,
+                 color=custom_colors['blue'], alpha=0.5, lw=0)
+ax3.set_ylim([0, 15])
+ax3.set_yticks([0,5,10,15])
+ax3.set_xlabel('Time [s]')
+ax3.set_ylabel('Presyn. ER [Hz]', color=custom_colors['blue'])
+ax3.set_xticks([0]+tics)
+ax3.spines['top'].set_visible(False)
+
+print("Max value of ER_pre")
+print(np.nanmax(brate_pre_meanER))
+
+
+ax3_tw = ax3.twinx()
+ax3_tw.set_yticks([0, 50])
+ax3_tw.set_ylim([0, 60])
+ax3_tw.plot(brate_pre[:, 0], 100*brate_pre_meanBP, color=custom_colors['red'], lw=1)
+ax3_tw.fill_between(brate_pre[:, 0],
+                    100*brate_pre_meanBP - n_SD*100*brate_pre_stdBP,
+                    100*brate_pre_meanBP + n_SD*100*brate_pre_stdBP,
+                    color=custom_colors['red'], alpha=0.5, lw=0)
+ax3_tw.set_ylabel('Presyn. BP [%]', color=custom_colors['red'])
+ax3_tw.spines['top'].set_visible(False)
+
+plt.tight_layout()
+plt.savefig(args.outfile_name)
+plt.close()
---
+++ sim_plasticity_rule.cpp
@@ -0,0 +1,338 @@
+/***********************************************************
+ Description: This program tests the burst-and-
+ event-dependent plasticity rule.
+ 
+ - Two populations of 2-compartment pyramidal neurons are
+ each subjected to Poisson spiking noise.
+ - Weights of the dendritic Poisson inputs onto the postsynaptic
+ population modulate the burst fraction of that population.
+ - The plastic weights are those that connect the presynaptic
+ pyr population to the soma of the postsynaptic population.
+************************************************************/
+
+#include "auryn.h"
+#include "EBCPConnection.h"
+#include "BCPConnection.h"
+#include "AdaptiveEBCPConnection.h"
+#include "BurstPoissonGroup.h"
+
+using namespace auryn;
+
+namespace po = boost::program_options;
+
+int main(int ac, char* av[])
+{
+    int errcode = 0;
+    int seed = 1;
+    double w0 = 0.8;
+    double d0 = 0.1;
+    double kappa = 1.0;
+    double tau_pre = 20.e-3;
+    double simtime = 1800;
+    double moving_average_time_constant = 4.;
+    std::string connect_type = "BCP";
+    string simname = "plasticity_rule";
+    std::string dir = ".";
+    
+    
+    /**************************************************/
+    /********              OPTIONS            *********/
+    /**************************************************/
+    try {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+        ("help", "produce help message")
+        ("d0", po::value<double>(), "dendritic input initial strength")
+        ("w0", po::value<double>(), "total input initial strength")
+        ("simtime", po::value<double>(), "simulation time")
+        ("kappa", po::value<double>(), "poisson group rate")
+        ("dir", po::value<string>(), "output directory")
+        ("seed", po::value<int>(), "random seed")
+        ("connect_type", po::value<string>(), "connection type: BCP or EBCP")
+        ("tau_pre", po::value<double>(), "presynaptic trace time constant")
+        ("alpha", po::value<double>(), "moving average time constant")
+        ("sim_name", po::value<string>(), "simulation name")
+        ;
+        
+        po::variables_map vm;
+        po::store(po::parse_command_line(ac, av, desc), vm);
+        po::notify(vm);
+        
+        if (vm.count("help")) {
+            std::cout << desc << "\n";
+            return 1;
+        }
+        
+        if (vm.count("d0")) {
+            std::cout << "d0 set to "
+            << vm["d0"].as<double>() << ".\n";
+            d0 = vm["d0"].as<double>();
+        }
+        
+        if (vm.count("w0")) {
+            std::cout << "w0 set to "
+            << vm["w0"].as<double>() << ".\n";
+            w0 = vm["w0"].as<double>();
+        }
+        
+        if (vm.count("kappa")) {
+            std::cout << "kappa set to "
+            << vm["kappa"].as<double>() << ".\n";
+            kappa = vm["kappa"].as<double>();
+        }
+        
+        if (vm.count("dir")) {
+            std::cout << "dir set to "
+            << vm["dir"].as<string>() << ".\n";
+            dir = vm["dir"].as<string>();
+        }
+        
+        if (vm.count("simtime")) {
+            std::cout << "simtime set to "
+            << vm["simtime"].as<double>() << ".\n";
+            simtime = vm["simtime"].as<double>();
+        }
+        
+        if (vm.count("seed")) {
+            std::cout << "seed set to "
+            << vm["seed"].as<int>() << ".\n";
+            seed = vm["seed"].as<int>();
+        }
+        
+        if (vm.count("connect_type")) {
+            std::cout << "connection type set to "
+            << vm["connect_type"].as<string>() << ".\n";
+            connect_type = vm["connect_type"].as<string>();
+        }
+        
+        if (vm.count("tau_pre")) {
+            std::cout << "presynaptic trace time constant set to "
+            << vm["tau_pre"].as<double>() << ".\n";
+            //tau_pre = vm["tau_pre"].as<double>();
+            tau_pre = 10.
+        }
+        
+        if (vm.count("alpha")) {
+            std::cout << "moving average time constant set to "
+            << vm["alpha"].as<double>() << ".\n";
+            //moving_average_time_constant = vm["alpha"].as<double>();
+            moving_average_time_constant = vm["tau_pre"].as<double>();
+        }
+        
+        if (vm.count("sim_name")) {
+            std::cout << "simulation name set to "
+            << vm["sim_name"].as<string>() << ".\n";
+            simname = vm["sim_name"].as<string>();
+        }
+    }
+    catch(std::exception& e) {
+        std::cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        std::cerr << "Exception of unknown type!\n";
+    }
+    
+    
+    // INITIALIZE AURYN AND SET THE SEED FOR REPRODUCIBILITY
+    auryn_init( ac, av, dir, simname);
+    sys->set_master_seed(seed);
+    
+    
+    /**************************************************/
+    /******          NEURON POPULATIONS       *********/
+    /**************************************************/
+    
+    // Main neuron group
+    NaudGroup * neurons_exc = new NaudGroup(1000);
+    neurons_exc->syn_exc_soma->set_nmda_ampa_current_ampl_ratio(0.);
+    neurons_exc->syn_exc_dend->set_nmda_ampa_current_ampl_ratio(0.);
+    //neurons_exc->syn_exc_soma->set_ampa_nmda_ratio(1);
+    //neurons_exc->syn_exc_dend->set_ampa_nmda_ratio(100);
+    
+    // Input group
+    //PoissonGroup * poisson_soma = new PoissonGroup(4000, kappa);
+    NaudGroup * input = new NaudGroup(1000);
+    input->syn_exc_soma->set_nmda_ampa_current_ampl_ratio(0.);
+    input->syn_exc_dend->set_nmda_ampa_current_ampl_ratio(0.);
+    input->random_mem(-70e-3, 5.e-3);
+    
+    // Noise groups
+        // dendritic noise for main neuron group
+    PoissonGroup * poisson_dend = new PoissonGroup(100, kappa);
+    
+        // noise for input neuron group
+    NeuronID nb_exc_Poisson_neurons = 25000;
+    NeuronID nb_inh_Poisson_neurons = nb_exc_Poisson_neurons;
+    float poisson_rate = 10.;
+    PoissonGroup* exc_Poisson = new PoissonGroup(nb_exc_Poisson_neurons, poisson_rate);
+    PoissonGroup* inh_Poisson = new PoissonGroup(nb_inh_Poisson_neurons, poisson_rate);
+    
+    
+    /**************************************************/
+    /******           CONNECTIVITY            *********/
+    /**************************************************/
+    //-- Connect input group to main group's somata with plastic synapses
+    const double sparseness = 0.2;
+    const double learning_rate = 3.5e-3;
+    const double max_weight = 1.0;
+    
+    BCPConnection * con_ext_soma;
+    
+    if (connect_type == "BCP") {
+        std::cout << "WARNING! CHANGING TAU_PRE NOT SUPPORTED!!! tau_pre = 20 ms"<<std::endl;
+        con_ext_soma = new BCPConnection(input, neurons_exc, w0,
+                                         sparseness, learning_rate, max_weight, GLUT);
+    }
+    else if (connect_type == "EBCP"){
+        con_ext_soma = new EBCPConnection(input, neurons_exc, w0,
+                                          sparseness, learning_rate, max_weight, tau_pre, GLUT);
+    }
+    else if (connect_type == "AdaptiveEBCP"){
+        
+        con_ext_soma = new AdaptiveEBCPConnection(input, neurons_exc, w0,
+                                                  sparseness, learning_rate, max_weight, tau_pre, GLUT);
+        con_ext_soma->set_post_trace_event_tau(moving_average_time_constant);
+        con_ext_soma->set_post_trace_burst_tau(moving_average_time_constant);
+
+    }
+    
+    std::cout << "presynaptic trace time constant set to "
+    << tau_pre << ".\n";
+
+    con_ext_soma->set_target("g_ampa");
+    
+    
+    //-- Connect first Poisson noise group to main group's dendrites
+    SparseConnection * con_ext_dend = new SparseConnection(poisson_dend, neurons_exc, d0, sparseness);
+    con_ext_dend->set_target("g_ampa_dend");
+    
+    //-- Connect noise to input population
+    int number_of_ext_conn[2] = {100, 30}; //exc, inh
+    
+    float p_exc = float(number_of_ext_conn[0])/float(nb_exc_Poisson_neurons);
+    float p_inh = float(number_of_ext_conn[1])/float(nb_inh_Poisson_neurons);
+    
+    const float w_exc = 0.17;   // conductance amplitude in units of leak conductance
+    const float w_inh = 0.26;
+    
+    SparseConnection * con_ext_exc_soma1 = new SparseConnection(exc_Poisson, input, w_exc, p_exc, GLUT);
+    con_ext_exc_soma1->set_target("g_ampa");
+    SparseConnection * con_ext_inh_soma1 = new SparseConnection(inh_Poisson, input, w_inh, p_inh, GABA);
+    con_ext_inh_soma1->set_target("g_gaba");
+    
+    const float w_exc_dend = 0.0425;        // conductance amplitude in units of leak conductance
+    const float w_inh_dend = 0.065;
+    
+    SparseConnection * con_ext_exc_dend1 = new SparseConnection(exc_Poisson, input, w_exc_dend, p_exc, GLUT);
+    con_ext_exc_dend1->set_target("g_ampa_dend");
+    SparseConnection * con_ext_inh_dend1 = new SparseConnection(inh_Poisson, input, w_inh_dend, p_inh, GABA);
+    con_ext_inh_dend1->set_target("g_gaba_dend");
+    
+    /**************************************************/
+    /******              MONITORS             *********/
+    /**************************************************/
+    const double binsize = 2.5; //std::max(moving_average_time_constant/5., 5.);
+    auto seed_str = std::to_string(seed);
+    sys->set_online_rate_monitor_target(neurons_exc);
+    sys->set_online_rate_monitor_tau(binsize);
+    SpikeMonitor * smon = new SpikeMonitor( neurons_exc, sys->fn("ras_seed"+seed_str) );
+    PopulationRateMonitor * pmon = new PopulationRateMonitor( neurons_exc, sys->fn("prate_seed"+seed_str), binsize );
+    BurstRateMonitor * brmon = new BurstRateMonitor( neurons_exc, sys->fn("brate_seed"+seed_str), binsize );
+    BurstRateMonitor * brmon_input = new BurstRateMonitor( input, sys->fn("brate_input_seed"+seed_str), binsize );
+
+    VoltageMonitor * vmon   = new VoltageMonitor( neurons_exc, 0, sys->fn("mem"), 1e-3);
+    StateMonitor * smon_vd  = new StateMonitor( neurons_exc, 0, "Vd", sys->fn("Vd") );
+    StateMonitor * smon_m   = new StateMonitor( neurons_exc, 0, "thr", sys->fn("thr") );
+    smon_m->record_for(10);
+    StateMonitor * smon_ws  = new StateMonitor( neurons_exc, 0, "wsoma", sys->fn("wsoma") );
+    smon_ws->record_for(10);
+    StateMonitor * smon_wd  = new StateMonitor( neurons_exc, 0, "wdend", sys->fn("wdend") );
+    smon_wd->record_for(10);
+    
+    WeightMonitor * wmon = new WeightMonitor( con_ext_soma, sys->fn("consyn"), binsize);
+    wmon->add_equally_spaced(100);
+    
+    WeightSumMonitor * wsmon = new WeightSumMonitor( con_ext_soma, sys->fn("wsum_seed"+seed_str), binsize );
+    
+    // Monitors for  estimating burst probability
+    std::vector< StateMonitor* > smon_tr_post_ev;
+    std::vector< StateMonitor* > smon_tr_post_b;
+    
+    for (int i=0;i<50;i++){
+        StateMonitor * ev = new StateMonitor( con_ext_soma->get_tr_event(), i, sys->fn(simname,i,"trevent_seed"+seed_str), binsize);
+        smon_tr_post_ev.push_back(ev);
+        StateMonitor * b = new StateMonitor( con_ext_soma->get_tr_burst(), i, sys->fn(simname,i,"trburst_seed"+seed_str), binsize);
+        smon_tr_post_b.push_back(b);
+        
+    }
+    /**************************************************/
+    /******         CURRENT INJECTORS         *********/
+    /**************************************************/
+    CurrentInjector *curr_input_pop = new CurrentInjector(input, "Vd");
+    CurrentInjector *curr_dend_main = new CurrentInjector(neurons_exc, "Vd");
+    curr_dend_main->set_all_currents(50.e-12/neurons_exc[0].get_Cd());
+    
+    /**************************************************/
+    /******             SIMULATION            *********/
+    /**************************************************/
+    logger->msg("Running ...",PROGRESS);
+    
+    // simulate
+    /*
+
+    double testtime = 10;
+    con_ext_soma->stdp_active = false;
+    sys->run(5*moving_average_time_constant);
+    con_ext_soma->stdp_active = true;
+    sys->run(4000.);
+     */
+    /*
+    vmon->record_for(20);
+    smon_vd->record_for(20);
+    sys->run(100.);
+    vmon->record_for(20);
+    smon_vd->record_for(20);
+    sys->run(100.);
+    vmon->record_for(20);
+    smon_vd->record_for(20);
+    sys->run(100.);
+    vmon->record_for(20);
+    smon_vd->record_for(20);
+    sys->run(100.);
+    smon_vd->record_for(20);
+    vmon->record_for(20);
+    sys->run(100.);
+*/
+    double testtime = 10;
+    
+    con_ext_soma->stdp_active = false;
+    con_ext_dend->set_all(1.0*d0);
+    sys->run(moving_average_time_constant*4);
+    
+    con_ext_soma->stdp_active = true;
+    sys->run(simtime);
+
+    con_ext_dend->set_all(1.5*d0);
+    sys->run(simtime);
+
+    con_ext_dend->set_all(1.0*d0);
+    sys->run(simtime);
+    
+    con_ext_dend->set_all(0.55*d0);
+    sys->run(simtime);
+    
+    con_ext_dend->set_all(1.0*d0);
+    sys->run(simtime);
+    
+    // test that plasticity is not affected by burst prob in input population
+    curr_input_pop->set_all_currents(40.e-12/input[0].get_Cd());
+    sys->run(simtime);
+    
+    if (errcode)
+        auryn_abort(errcode);
+    
+    logger->msg("Freeing ...",PROGRESS,true);
+    auryn_free();
+    return errcode;
+}
